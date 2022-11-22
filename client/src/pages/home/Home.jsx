4115981839bba4555c00247bfc86c26f76bb1a73
Lauren <lburke522@gmail.com>
2022-11-22 09:38:34 -0500

    home page complete

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './home.scss';
+import Nav from '../../components/nav/Nav'
+import Featured from '../../components/featured/Featured';
+import List from '../../components/list/List';
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <Nav />
+       <Featured />
+       <List />
+       <List />
+       <List />
+       <List />
+       <List />
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/pages/watch/Watch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './watch.scss';
+
+const Watch = () => {
+  return (
+    <div className='watch'>
+      
+    </div>
+  )
+}
+
+export default Watch
